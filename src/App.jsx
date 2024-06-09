f1ed31f7cc6b835574611dd6d7069c954243a5a6
Manpreet Singh <manpreetmss400@gmail.com>
2024-06-09 06:08:00 +0530

    hero section designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,15 @@
-import { useState } from "react";
+import Main from "./components/Main";
+import Nav from "./components/Nav";
+
 
 
 export default function App() {
-  const [isExpanded, setIsExpanded] = useState(false);
 
-  function handleMenu() {
-    setIsExpanded(!isExpanded);
-  }
 
   return (
-    <nav className="p-3 flex bg-white justify-between items-center">
-      <a href="#" id="brand" className="flex gap-2 items-center">
-        <img src="vite.svg" alt="logo" className="object-cover max-w-12 max-h-12" />
-        <span className="text-lg font-medium font-display">ToDesktop</span>
-      </a>
-
-      <div className="nav-menu hidden lg:flex gap-12">
-        <a href="#" className="font-medium hover:text-yellow-500">Pricing</a>
-        <a href="#" className="font-medium hover:text-yellow-500">Terms</a>
-        <a href="#" className="font-medium hover:text-yellow-500">Contact Us</a>
-        <a href="#" className="font-medium hover:text-yellow-500">About</a>
-      </div>
-
-      <button className="hidden lg:flex gap-2 items-center border border-gray-400 px-6 py-2 rounded-lg hover:border-gray-600">
-        <img src="vite.svg" alt="logo" className="object-cover max-w-12 max-h-12" />
-        <span>Electron developers</span>
-        <i className="fa-solid fa-arrow-right"></i>
-      </button>
-
-      <button className="p-2 lg:hidden" onClick={handleMenu}>
-        <i className="fa-solid fa-bars text-gray-600"></i>
-      </button>
-
-      <div className={`fixed lg:hidden bg-white  inset-0 ${isExpanded ? "block" : "hidden"}`}>
-        <div className="nav-bar flex justify-between p-3">
-          <a href="#" id="brand" className="flex gap-2 items-center">
-            <img src="vite.svg" alt="logo" className="object-cover max-w-12 max-h-12" />
-            <span className="text-lg font-medium font-display">ToDesktop</span>
-          </a>
-          <button className="p-2 lg:hidden" onClick={handleMenu}>
-            <i className="fa-solid fa-close text-gray-600"></i>
-          </button>
-        </div>
-
-        <div className="nav-menu mt-6 flex flex-col">
-          <a href="#" className="block rounded-lg font-medium hover:text-yellow-500 p-4 m-3 hover:bg-gray-50">Pricing</a>
-          <a href="#" className="block rounded-lg font-medium hover:text-yellow-500 p-4 m-3 hover:bg-gray-50">Terms</a>
-          <a href="#" className="block rounded-lg font-medium hover:text-yellow-500 p-4 m-3 hover:bg-gray-50">Contact Us</a>
-          <a href="#" className="block  rounded-lg font-medium hover:text-yellow-500 p-4 m-3 hover:bg-gray-50">About</a>
-        </div>
-
-        <div className="h-1 bg-gray-300"></div>
-
-        <button className="w-full mt-6 flex gap-2 items-center hover:bg-gray-50 px-6 py-4 rounded-lg hover:border-gray-600">
-          <img src="vite.svg" alt="logo" className="object-cover max-w-12 max-h-12" />
-          <span>Electron developers</span>
-        </button>
-      </div>
-    </nav>
+    <>
+      <Nav />
+      <Main />
+    </>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,39 @@
+
+
+import React from 'react';
+
+export default function Main() {
+    return (
+        <div id="hero" className='min-h-screen bg-gradient-to-br from-purple-50 via-orange-50 to-transparent'>
+            <div id="hero-container" className='sm:pt-5 max-w-4xl mx-auto px-4 pt-6 pb-32 flex flex-col sm:items-center sm:text-center'>
+                <div id="version-text" className='flex items-center my-6 gap-2 border border-yellow-300 bg-yellow-50 rounded-lg px-3 py-1 w-fit shadow-md hover:shadow-lg hover:-translate-y-1 transition duration-300 group'>
+                    <div className="w-2 h-2 bg-yellow-400 rounded-full border border-yellow-600"></div>
+                    <p className='font-display font-medium text-yellow-600'>v0.21.1 <span className='text-yellow-800'>Find-in-page bug fixes</span></p>
+                    <i className="fa-solid fa-arrow-right text-yellow-600 group-hover:translate-x-1 transition duration-300"></i>
+                </div>
+
+                <div id="hero-feature" className="hidden sm:flex gap-8 my-6">
+                    <div id="hero-feature" className='flex justify-center gap-2 items-center text-gray-500'>
+                        <i className='fa-solid fa-file-code text-sm'></i>
+                        <p>Code Optional</p>
+                    </div>
+                    <div id="hero-feature" className='flex justify-center gap-2 items-center text-gray-500'>
+                        <i className='fa-solid fa-hand-back-fist text-sm'></i>
+                        <p>Drag & drop builder</p>
+                    </div>
+                    <div id="hero-feature" className='flex justify-center gap-2 items-center text-gray-500'>
+                        <i className='fa-solid fa-laptop text-sm'></i>
+                        <p>Windows, Mac, Linux</p>
+                    </div>
+                </div>
+
+                <h1 className='text-4xl font-semibold leading-snug sm:leading-normal mt-4 sm:text-6xl'>Web app to desktop app in minutes</h1>
+                <p className='text-gray-800 text-xl mt-4 sm:text-2xl sm:mt-8'>Take your web app codebase and transform it into a cross platform desktop app with native functionality.</p>
+                <div id="button-container" className='mt-12 flex flex-col gap-4 sm:flex-row'>
+                    <button className='px-8 py-3 font-semibold rounded-lg text-white bg-violet-700 shadow-sm hover:bg-opacity-90'>Download now</button>
+                    <button className='px-8 py-3 font-semibold rounded-lg bg-white border border-gray-400 shadow-sm hover:border-gray-800'>Read Docs</button>
+                </div>
+            </div>
+        </div>
+    );
+}
