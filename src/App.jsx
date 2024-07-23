fcfc0359594af40bc277bbbca52c77add569020c
Manpreet Singh <manpreetmss400@gmail.com>
2024-07-23 18:32:49 +0530

    result summary component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Card from "./components/Card";
 export default function App() {
   return (
     <div
-      className="relative bg-[#0a0c1b] w-screen h-screen flex justify-center items-center"
+      className="relative w-screen h-screen flex justify-center items-center"
     >
       <Card />
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,40 +1,63 @@
-
-import mobileImg from '../images/image-header-mobile.jpg';
-import desktopImg from '../images/image-header-desktop.jpg';
+import reactionLogo from '../images/icon-reaction.svg';
+import memoryLogo from '../images/icon-memory.svg';
+import verbalLogo from '../images/icon-verbal.svg';
+import visualLogo from '../images/icon-visual.svg';
 
 
 function Card() {
     return (
         <div
-            className="font-inter overflow-hidden bg-[#1c1938] font-outfit relative rounded-xl shadow-2xl w-[310px] md:w-[900px] md:h-[360px] block md:flex"
+            className="font-hanken-grotesk flex flex-col sm:flex-row text-black bg-white w-[350px] sm:w-[500px] sm:rounded-3xl sm:shadow-2xl"
         >
-            <div className='img-container md:order-1 w-full h-[200px] md:w-[940px] md:h-[360px]'>
-                <img className='md:hidden h-full w-full' src={mobileImg} alt="img mobile" />
-                <img className='hidden md:block h-full w-full' src={desktopImg} alt="img desktop" />
+            <div className="flex flex-col sm:w-[50%] text-white text-center items-center w-full bg-blue-600 rounded-b-3xl sm:rounded-t-3xl">
+                <div className="my-4">Your Result</div>
+
+                <div className="w-28 aspect-square rounded-full bg-blue-700 flex flex-col justify-center items-center">
+                    <span className="font-bold text-4xl">76</span>
+                    <span className="text-gray-400 text-xs">of 100</span>
+                </div>
+
+                <div className="mt-4 font-2xl font-bold">Great</div>
+
+                <p className="mx-16 sm:mx-10 sm:text-xs mt-1 mb-4 text-sm text-gray-300">You scored higher than 65% of the people who have taken these tests.</p>
             </div>
 
-            <div className='p-4 pt-8 md:p-14 text-white text-center md:text-left font-lexend-deca'>
-                <h1 className='md:max-w-[315px] text-2xl md:text-[28px] font-semibold'>
-                    <span className='pl-6 md:pl-0'></span>Get <span className='text-[#a460d5] font-bold'>insights</span> that help your business grow.
-                </h1>
+            <div className="flex flex-col sm:w-[50%]">
+                <h1 className="m-4 sm:ml-6 text-sm font-semibold">Summary</h1>
 
-                <p className='px-4 md:px-0 mt-4 md:mt-8 md:pr-16 leading-5 text-center md:text-left text-sm md:text-md text-gray-500 font-semibold'>
-                    <span className='pl-4 md:pl-0'></span>Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.
-                </p>
+                <div className="px-6 flex flex-col gap-y-4 text-xs font-semibold">
+                    <div className="py-3 px-4 bg-red-50 rounded-md flex justify-between">
+                        <span className='flex gap-x-2'>
+                            <img className='w-4 h-4' src={reactionLogo} alt="reaction" />
+                            <span className='text-red-400'>Reaction</span>
+                        </span>
+                        <span>80 <span className="ml-1 text-gray-400">/100</span></span>
+                    </div>
+                    <div className="py-3 px-4 bg-yellow-50 rounded-md flex justify-between">
+                        <span className="flex gap-x-2">
+                            <img className='w-4 h-4' src={memoryLogo} alt="logo" />
+                            <span className='text-yellow-300'>Memory</span>
+                        </span>
+                        <span>92 <span className="ml-1 text-gray-400">/100</span></span>
+                    </div>
+                    <div className="py-3 px-4 bg-[#f2fbfa] rounded-md flex justify-between">
+                        <span className="flex gap-x-2">
+                            <img className='w-4 h-4' src={verbalLogo} alt="logo" />
+                            <span className='text-[#26a688]'>Verbal</span>
+                        </span>
+                        <span> 61 <span className="ml-1 text-gray-400">/100</span></span>
+                    </div>
+                    <div className="py-3 px-4 bg-blue-50 rounded-md flex justify-between">
+                        <span className="flex gap-x-2">
+                            <img className='w-4 h-4' src={visualLogo} alt="logo" />
+                            <span className='text-blue-300'>Visual</span>
+                        </span>
+                        <span>72 <span className="ml-1 text-gray-400">/100</span></span>
+                    </div>
+                </div>
 
-                <div className='mt-2 md:mt-8 p-4 md:px-0 max-w-[290px] flex flex-col md:flex-row items-center md:items-start md:justify-between gap-y-4'>
-                    <div className='flex flex-col'>
-                        <span className='text-xl font-semibold'>10k+</span>
-                        <span className='text-gray-500 font-semibold text-xs uppercase'>companies</span>
-                    </div>
-                    <div className='flex flex-col'>
-                        <span className='text-xl font-semibold'>314</span>
-                        <span className='text-gray-500 font-semibold text-xs uppercase'>templates</span>
-                    </div>
-                    <div className='flex flex-col'>
-                        <span className='text-xl font-semibold uppercase'>12m+</span>
-                        <span className='text-gray-500 uppercase font-semibold text-xs'>queries</span>
-                    </div>
+                <div className='flex justify-center my-6'>
+                    <button className='py-2 px-16 text-sm font-semibold text-white bg-[#303b59] hover:bg-blue-500 rounded-3xl'>Continue</button>
                 </div>
             </div>
         </div>
