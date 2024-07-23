3d53131ec52fac2296778061deab021744371ad4
Manpreet Singh <manpreetmss400@gmail.com>
2024-07-23 15:53:03 +0530

    nft-preview-card-component

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,9 @@ import Card from "./components/Card";
 export default function App() {
   return (
     <div
-      className="relative bg-[#19a1ad] w-screen h-screen flex justify-center items-center"
+      className="relative bg-[#0d1a2d] w-screen h-screen flex justify-center items-center"
     >
-
-      <div className="absolute top-0 -left-[100px] md:left-[0px] w-[400px] md:w-[600px] h-[400px] md:h-[500px] rotate-[175deg] md:rotate-[180deg] pattern-top"></div>
-
       <Card />
-
-      <div className="absolute bottom-0 -right-[100px] md:right-0 w-[400px] md:w-[600px] h-[400px] pattern-bottom"></div>
-      
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,35 +1,50 @@
-import victorImg from '../images/image-victor.jpg';
-import img5 from '../images/favicon-32x32.png';
+import equilibriumImg from '../images/image-equilibrium.jpg';
+import clockImg from '../images/icon-clock.svg';
+import ethereumImg from '../images/icon-ethereum.svg';
+import avatarImg from '../images/image-avatar.png';
+import viewImg from '../images/icon-view.svg';
 
 function Card() {
     return (
-        <div className="kumbh-sans relative p-10 rounded-xl overflow-hidden flex flex-col items-center justify-center w-[310px] bg-transparent z-[2] bg-white h-[340px]">
-            <div className="absolute top-0 inset-x-0 h-[38%] z-[-1] pattern-card"></div>
+        <div
+            className="bg-[#15273f] text-[#687c9b] font-outfit relative p-6 rounded-xl shadow-2xl w-[310px]"
+        >
+            <div className='cube-img-container'>
+                <img
+                    className='rounded-xl hover:cursor-pointer'
+                    src={equilibriumImg}
+                    alt="dice"
+                />
+            </div>
 
-            <img className='mt-[60px] rounded-full border-4 border-white' src={victorImg} alt="victor profile pic" />
+            <h1 className='text-white hover:text-[#04faf6] hover:cursor-pointer font-semibold text-xl my-4'>Equilibrium #3429</h1>
+            <p className='text-md'>Our Equilibrium collection promotes balance and calm.</p>
 
-            <h1 className='flex items-baseline gap-x-2'>
-                <span className='mt-4 font-bold'>Victor Crest</span>
-                <span className='text-gray-500'>26</span>
-            </h1>
-            
-            <p className='mt-1 text-xs text-gray-500'>London</p>
-
-            <div className='absolute top-[72%] border border-t-gray-50 w-full'></div>
-
-            <div className='flex justify-between mt-12 w-full px-1'>
-                <div className='flex flex-col items-center justify-center'>
-                    <span className='font-semibold text-gray-800'>80K</span>
-                    <span className='text-xs text-gray-500'>Followers</span>
+            <div className='flex justify-between font-semibold mt-4'>
+                <div className='flex gap-x-2 items-center'>
+                    <img src={ethereumImg} alt="ethereum" />
+                    <span className='text-[#04faf6]'>0.041 ETH</span>
                 </div>
-                <div className='flex flex-col items-center justify-center'>
-                    <span className='font-semibold text-gray-800'>803K</span>
-                    <span className='text-xs text-gray-500'>Likes</span>
+
+                <div className='flex gap-x-2 items-center text-sm'>
+                    <img src={clockImg} alt="clock" />
+                    <span>3 days left</span>
                 </div>
-                <div className='flex flex-col items-center justify-center'>
-                    <span className='font-semibold text-gray-800'>1.4K</span>
-                    <span className='text-xs text-gray-500'>Photos</span>
-                </div>
+            </div>
+
+            <hr className='border-gray-700 my-4' />
+
+            <div className='flex items-center space-x-4'>
+                <img
+                    className='w-8 h-8 border border-white rounded-full'
+                    src={avatarImg}
+                    alt="profile pic"
+                />
+
+                <span>
+                    <span>Creation of</span>
+                    <span className='text-white ml-1.5 hover:text-[#04faf6] hover:cursor-pointer'>Jules Wyvern</span>
+                </span>
             </div>
 
         </div>
