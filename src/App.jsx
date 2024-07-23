5493d99202c571f60c0ec191580f08c45a2b6008
Manpreet Singh <manpreetmss400@gmail.com>
2024-07-23 17:33:44 +0530

    stats-preview-card-component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Card from "./components/Card";
 export default function App() {
   return (
     <div
-      className="relative bg-[#0d1a2d] w-screen h-screen flex justify-center items-center"
+      className="relative bg-[#0a0c1b] w-screen h-screen flex justify-center items-center"
     >
       <Card />
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,52 +1,42 @@
-import equilibriumImg from '../images/image-equilibrium.jpg';
-import clockImg from '../images/icon-clock.svg';
-import ethereumImg from '../images/icon-ethereum.svg';
-import avatarImg from '../images/image-avatar.png';
-import viewImg from '../images/icon-view.svg';
+
+import mobileImg from '../images/image-header-mobile.jpg';
+import desktopImg from '../images/image-header-desktop.jpg';
+
 
 function Card() {
     return (
         <div
-            className="bg-[#15273f] text-[#687c9b] font-outfit relative p-6 rounded-xl shadow-2xl w-[310px]"
+            className="font-inter overflow-hidden bg-[#1c1938] font-outfit relative rounded-xl shadow-2xl w-[310px] md:w-[900px] md:h-[360px] block md:flex"
         >
-            <div className='cube-img-container'>
-                <img
-                    className='rounded-xl hover:cursor-pointer'
-                    src={equilibriumImg}
-                    alt="dice"
-                />
+            <div className='img-container md:order-1 w-full h-[200px] md:w-[940px] md:h-[360px]'>
+                <img className='md:hidden h-full w-full' src={mobileImg} alt="img mobile" />
+                <img className='hidden md:block h-full w-full' src={desktopImg} alt="img desktop" />
             </div>
 
-            <h1 className='text-white hover:text-[#04faf6] hover:cursor-pointer font-semibold text-xl my-4'>Equilibrium #3429</h1>
-            <p className='text-md'>Our Equilibrium collection promotes balance and calm.</p>
+            <div className='p-4 pt-8 md:p-14 text-white text-center md:text-left font-lexend-deca'>
+                <h1 className='md:max-w-[315px] text-2xl md:text-[28px] font-semibold'>
+                    <span className='pl-6 md:pl-0'></span>Get <span className='text-[#a460d5] font-bold'>insights</span> that help your business grow.
+                </h1>
 
-            <div className='flex justify-between font-semibold mt-4'>
-                <div className='flex gap-x-2 items-center'>
-                    <img src={ethereumImg} alt="ethereum" />
-                    <span className='text-[#04faf6]'>0.041 ETH</span>
+                <p className='px-4 md:px-0 mt-4 md:mt-8 md:pr-16 leading-5 text-center md:text-left text-sm md:text-md text-gray-500 font-semibold'>
+                    <span className='pl-4 md:pl-0'></span>Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.
+                </p>
+
+                <div className='mt-2 md:mt-8 p-4 md:px-0 max-w-[290px] flex flex-col md:flex-row items-center md:items-start md:justify-between gap-y-4'>
+                    <div className='flex flex-col'>
+                        <span className='text-xl font-semibold'>10k+</span>
+                        <span className='text-gray-500 font-semibold text-xs uppercase'>companies</span>
+                    </div>
+                    <div className='flex flex-col'>
+                        <span className='text-xl font-semibold'>314</span>
+                        <span className='text-gray-500 font-semibold text-xs uppercase'>templates</span>
+                    </div>
+                    <div className='flex flex-col'>
+                        <span className='text-xl font-semibold uppercase'>12m+</span>
+                        <span className='text-gray-500 uppercase font-semibold text-xs'>queries</span>
+                    </div>
                 </div>
-
-                <div className='flex gap-x-2 items-center text-sm'>
-                    <img src={clockImg} alt="clock" />
-                    <span>3 days left</span>
-                </div>
             </div>
-
-            <hr className='border-gray-700 my-4' />
-
-            <div className='flex items-center space-x-4'>
-                <img
-                    className='w-8 h-8 border border-white rounded-full'
-                    src={avatarImg}
-                    alt="profile pic"
-                />
-
-                <span>
-                    <span>Creation of</span>
-                    <span className='text-white ml-1.5 hover:text-[#04faf6] hover:cursor-pointer'>Jules Wyvern</span>
-                </span>
-            </div>
-
         </div>
     );
 }
