2d86b86dbce1f2242f89e57f36b581983d405392
Manpreet Singh <manpreetmss400@gmail.com>
2024-07-25 17:58:02 +0530

    3column-preview-card

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Card from "./components/Card";
 export default function App() {
   return (
     <div
-      className="relative w-screen h-screen flex justify-center items-center"
+      className="relative w-screen min-h-screen flex justify-center items-center"
     >
       <Card />
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,65 +1,50 @@
-import reactionLogo from '../images/icon-reaction.svg';
-import memoryLogo from '../images/icon-memory.svg';
-import verbalLogo from '../images/icon-verbal.svg';
-import visualLogo from '../images/icon-visual.svg';
+import iconLuxury from '../images/icon-luxury.svg';
+import iconSedans from '../images/icon-sedans.svg';
+import iconSuvs from '../images/icon-suvs.svg';
 
+function CardItem({ icon, title, para, color, btnClass }) {
+    const bgClass = `bg-[${color}]`;
+
+    return (
+        <div className={`w-fit p-10 md:pt-8 md:max-w-[250px] ${bgClass}`}>
+            <img src={icon} alt='icon' />
+            <h1 className='font-big-shoulders-display font-bold text-3xl my-8 uppercase'>{title}</h1>
+            <p className='text-xs text-[#c9c8c8] leading-5'>{para}</p>
+            <button className={`mt-8 md:mt-16 py-2 px-6 text-sm bg-white hover:bg-transparent hover:outline-2 hover:outline hover:text-white ${btnClass} rounded-3xl`}>Learn More</button>
+        </div>
+    );
+}
 
 function Card() {
+    const para1 = "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.";
+    const para2 = "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.";
+    const para3 = "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.";
+
     return (
         <div
-            className="font-hanken-grotesk flex flex-col sm:flex-row text-black bg-white w-[350px] sm:w-[500px] sm:rounded-3xl sm:shadow-2xl"
+            className="m-8 font-lexend-deca flex flex-col md:flex-row rounded-md overflow-hidden text-white"
         >
-            <div className="flex flex-col sm:w-[50%] text-white text-center items-center w-full bg-blue-600 rounded-b-3xl sm:rounded-t-3xl">
-                <div className="my-4">Your Result</div>
-
-                <div className="w-28 aspect-square rounded-full bg-blue-700 flex flex-col justify-center items-center">
-                    <span className="font-bold text-4xl">76</span>
-                    <span className="text-gray-400 text-xs">of 100</span>
-                </div>
-
-                <div className="mt-4 font-2xl font-bold">Great</div>
-
-                <p className="mx-16 sm:mx-10 sm:text-xs mt-1 mb-4 text-sm text-gray-300">You scored higher than 65% of the people who have taken these tests.</p>
-            </div>
-
-            <div className="flex flex-col sm:w-[50%]">
-                <h1 className="m-4 sm:ml-6 text-sm font-semibold">Summary</h1>
-
-                <div className="px-6 flex flex-col gap-y-4 text-xs font-semibold">
-                    <div className="py-3 px-4 bg-red-50 rounded-md flex justify-between">
-                        <span className='flex gap-x-2'>
-                            <img className='w-4 h-4' src={reactionLogo} alt="reaction" />
-                            <span className='text-red-400'>Reaction</span>
-                        </span>
-                        <span>80 <span className="ml-1 text-gray-400">/100</span></span>
-                    </div>
-                    <div className="py-3 px-4 bg-yellow-50 rounded-md flex justify-between">
-                        <span className="flex gap-x-2">
-                            <img className='w-4 h-4' src={memoryLogo} alt="logo" />
-                            <span className='text-yellow-300'>Memory</span>
-                        </span>
-                        <span>92 <span className="ml-1 text-gray-400">/100</span></span>
-                    </div>
-                    <div className="py-3 px-4 bg-[#f2fbfa] rounded-md flex justify-between">
-                        <span className="flex gap-x-2">
-                            <img className='w-4 h-4' src={verbalLogo} alt="logo" />
-                            <span className='text-[#26a688]'>Verbal</span>
-                        </span>
-                        <span> 61 <span className="ml-1 text-gray-400">/100</span></span>
-                    </div>
-                    <div className="py-3 px-4 bg-blue-50 rounded-md flex justify-between">
-                        <span className="flex gap-x-2">
-                            <img className='w-4 h-4' src={visualLogo} alt="logo" />
-                            <span className='text-blue-300'>Visual</span>
-                        </span>
-                        <span>72 <span className="ml-1 text-gray-400">/100</span></span>
-                    </div>
-                </div>
-
-                <div className='flex justify-center my-6'>
-                    <button className='py-2 px-16 text-sm font-semibold text-white bg-[#303b59] hover:bg-blue-500 rounded-3xl'>Continue</button>
-                </div>
-            </div>
+            <CardItem
+                title='Sedans'
+                para={para1}
+                icon={iconSedans}
+                color='#e28525'
+                btnClass="text-[#e28525]"
+            />
+            <CardItem
+                title='Suvs'
+                para={para2}
+                icon={iconSuvs}
+                color='#016972'
+                btnClass="text-[#016972]"
+            />
+            <CardItem
+                title="Luxury"
+                para={para3}
+                icon={iconLuxury}
+                color='#00403f'
+                btnClass="text-[#00403f]"
+            />
         </div>
     );
 }
