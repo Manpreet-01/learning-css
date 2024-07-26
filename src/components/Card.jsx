38250583edf6341e5e21820bdb837a27c124f173
Manpreet Singh <manpreetmss400@gmail.com>
2024-07-26 12:04:47 +0530

    huddle-landing-page-with-a-single-introductory-section styles improved

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,18 +13,17 @@ function Card() {
                     <p className='px-16 md:px-0 text-gray-200 text-lg'>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion. </p>
 
                     <button
-                        className='mt-4 py-3 w-[250px] md:w-[200px] shadow-black shadow-2xl rounded-3xl bg-white'
-                        style={{ color: 'hsl(257, 40%, 49%)' }}
+                        className='mt-4 py-3 w-[250px] md:w-[200px] text-[#674baf] shadow-black shadow-2xl rounded-3xl bg-white hover:bg-transparent hover:text-white hover:outline hover:outline-white'
                     >
                         Register
                     </button>
                 </div>
             </div>
 
-            <div className='mt-24 md:mt-16 mb-10 md:mr-24 flex gap-x-5 justify-center md:justify-end items-center text-[30px] text-white'>
-                <button className=''><i className="fa-brands fa-facebook"></i></button>
-                <button><i className="fa-brands fa-twitter "></i></button>
-                <button><i className="fa-brands fa-instagram"></i></button>
+            <div className='mt-24 md:mt-16 mb-10 md:mr-24 flex gap-x-5 justify-center md:justify-end items-center text-[20px] text-white'>
+                <button className='border-2 w-12 h-12 border-white hover:bg-white hover:text-[#674baf] rounded-full'><i className="fa-brands fa-facebook-f"></i></button>
+                <button className='border-2 w-12 h-12 border-white hover:bg-white hover:text-[#674baf] rounded-full'><i className="fa-brands fa-twitter"></i></button>
+                <button className='border-2 w-12 h-12 border-white hover:bg-white hover:text-[#674baf] rounded-full'><i className="fa-brands fa-instagram"></i></button>
             </div>
         </div>
     );
