d0971d35250ceb5a7f0603c01a29a00ba23a3435
Manpreet Singh <manpreetmss400@gmail.com>
2024-07-27 20:05:25 +0530

    intro-component-with-signup-form

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,48 +1,29 @@
-import img from '../images/logo.svg';
-import illustrationDashboard from '../images/illustration-dashboard.png';
-
 function Card() {
     return (
-        <div className='flex flex-col items-center px-8 md:w-[75%]'>
-            <div className='mt-28 md:mt-16 mb-8'>
-                <img src={img} alt="logo" />
+        <div className="bg-red-400 bg-img px-8 py-24 md:px-24 grid md:grid-cols-2 md:gap-x-12 md:place-content-center max-w-[1200px] text-white leading-relaxed">
+            <div className="grid text-center md:text-start md:place-content-center gap-y-8">
+                <h1 className="text-3xl md:text-4xl font-bold">Learn to code by watching others</h1>
+                <p className="text-gray-200 text-center md:text-start font-xs">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
             </div>
 
-            <h1 className='text-gray-500 text-3xl'>We are launching <span className='text-black'>soon!</span></h1>
+            <div className="grid place-content-start gap-y-4 mt-16 md:mt-0">
+                <button className="bg-violet-800 hover:bg-violet-700 hover:scale-[1.02] py-4 px-28 md:px-16 leading-relaxed rounded-md text-xs text-white">
+                    <span className="font-bold">Try it free 7 days</span> then $20/mo. thereafter
+                </button>
 
-            <p className='my-4 tesxt-md'>Subscribe to get notified</p>
-
-            <div className="flex flex-col md:flex-row md:items-start w-full md:w-[800px] px-8 mt-4 gap-6">
-                <div>
-                    <input className='email w-full md:w-[500px] pl-8 border py-2 rounded-3xl' placeholder='Your email address' type="email" name="email" id="email" />
-                    <p className='email-error hidden pt-2 text-sm text-center text-red-500'>Please provide a valid email address</p>
+                <div className="bg-white p-8 grid gap-y-2 rounded-md">
+                    <form className="grid gap-y-4">
+                        <input className="py-3 border-gray-400 rounded-md border placeholder:pl-8 placeholder:text-xs placeholder:font-bold" placeholder="First Name" type="text" />
+                        <input className="py-3 rounded-md border placeholder:pl-8 placeholder:text-xs placeholder:font-bold" placeholder="Last Name" type="text" />
+                        <input className="py-3 rounded-md border placeholder:pl-8 placeholder:text-xs placeholder:font-bold" placeholder="Email Address" type="email" />
+                        <input className="py-3 rounded-md border placeholder:pl-8 placeholder:text-xs placeholder:font-bold" placeholder="Password" type="password" />
+                        <button className="bg-green-500 hover:bg-green-400 hover:scale-[1.02] rounded-md py-4 text-xs font-bold uppercase">Claim your free trial</button>
+                    </form>
+                    
+                    <p className="text-gray-500 px-4 text-xs text-center leading-relaxed">By clicking the button, you are agreeing to our <span className="text-red-400 hover:text-red-500 text-sm hover:underline hover:cursor-pointer font-bold">Terms and Services</span></p>
                 </div>
-                <button className='w-full md:w-[200px]  text-white text-sm font-semibold border py-3 rounded-3xl bg-blue-600 hover:bg-blue-500'>Notify Me</button>
+
             </div>
-
-
-            <div className='mt-28 md:mt-16'>
-                <img src={illustrationDashboard} alt="illustratioin-dashboard" />
-            </div>
-
-
-            <div className='my-24 md:my-16 flex justify-center gap-4 items-center text-blue-500 text-xl md:text-5xl'>
-                <a className='hover:text-white hover:bg-blue-500 flex justify-center items-center rounded-full border border-gray-300 w-8 h-8 md:w-16 md:h-16 p-1 pt-[5px] pl-[9px] md:pt-[5px] md:pl-[15px]' href="#">
-                    <i class="w-full h-full fa-brands fa-facebook-f"></i>
-                </a>
-                <a className='hover:text-white hover:bg-blue-500 flex justify-center items-center rounded-full border border-gray-300 w-8 h-8 md:w-16 md:h-16 p-1 pt-[4px] pl-[4px] md:pt-[5px] md:pl-[5px]' href="#">
-                    <i class="fa-brands fa-twitter"></i>
-                </a>
-                <a className='hover:text-white hover:bg-blue-500 flex justify-center items-center rounded-full border border-gray-300 w-8 h-8 md:w-16 md:h-16 p-1 pt-[5px] pl-[4px]' href="#">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </div>
-
-
-            <footer className='text-gray-500 my-8 md:pb-16'>
-                &copy; Copyright Ping. All rights reserved.
-            </footer>
-
         </div>
     );
 }
