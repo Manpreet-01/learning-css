b30f965cc43e004129fb5c8199d8bd7a2412c2d6
Manpreet Singh <manpreetmss400@gmail.com>
2024-07-27 10:48:43 +0530

    four-card-feature-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import Card from "./components/Card";
-import logo from './images/logo.svg';
-import bgMobile from './images/bg-mobile.svg'
-import bgPc from './images/bg-desktop.svg'
+
 
 export default function App() {
   return (
     <div
-      className="relative w-screen h-screen z-[0] overflow-x-hidden"
-      style={{ backgroundColor: 'hsl(257, 40%, 49%)' }}
-    >      
-      <img className="-z-[1] block md:hidden absolute inset-0 w-full h-full" src={bgMobile} alt="background-image" />
-      <img className="-z-[1] hidden md:block absolute inset-0 w-full h-full" src={bgPc} alt="background-image" />
-
-      <img className='p-8 h-[105px]' src={logo} alt="logo" />
-
+      className="relative w-screen h-screen overflow-x-hidden"
+    >
       <Card />
     </div>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,38 @@
-import img from '../images/illustration-mockups.svg';
+import supervisorIcon from '../images/icon-supervisor.svg';
+import calculatorIcon from '../images/icon-calculator.svg';
+import karmaIcon from '../images/icon-karma.svg';
+import teamBuilderIcon from '../images/icon-team-builder.svg';
+
+
+function MiniCard({ title, para, icon, className }) {
+    return (
+        <div className={`space-y-4 md:space-y-3 md:w-[300px] p-8 md:pr-4 md:pb-2 border-t-4 rounded-xl shadow-2xl ${className}`}>
+            <h2 className='text-2xl md:text-sm font-semibold'>{title}</h2>
+            <p className='text-gray-500 md:text-xs pr-20 md:pr-4'>{para}</p>
+            <div className='flex justify-end pr-4 pt-8 md:py-4'>
+                <img src={icon} alt="icon" className='h-20 md:h-12' />
+            </div>
+        </div>
+    );
+}
 
 function Card() {
     return (
-        <div>
-            <div className='flex flex-col md:gap-x-12 md:px-16 md:flex-row items-center md:items-start md:justify-between'>
-                <div className='h-[280px] md:h-[400px] md:flex-1'>
-                    <img className='mt-16 md:mt-10 w-full h-full' src={img} alt="mockup image" />
+        <div className="pt-20 md:pt-8 px-8 md:px-0 font-poppins md:flex md:flex-col md:items-center">
+            <h1 className="text-2xl space-y-2 my-6 md:m-4">
+                <span className="block text-gray-500">Reliable, efficient delivery</span>
+                <span className="font-bold">Powered by Technology</span>
+            </h1>
+
+            <p className="md:text-center md:w-[700px] text-xl md:text-sm text-gray-500">Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
+
+            <div className='mt-36 md:mt-0 pb-16 space-y-8 md:flex md:items-center md:gap-8'>
+                <MiniCard className={"border-t-sky-500"} title={"Supervisor"} para={"Monitors activity to identify project roadblocks"} icon={supervisorIcon} />
+                <div className='space-y-4'>
+                    <MiniCard className={"border-t-red-500"} title={"Team Builder"} para={"Scans our talent network to create the optimal team for your project"} icon={teamBuilderIcon} />
+                    <MiniCard className={"border-t-yellow-500"} title={"Karma"} para={"Regularly evaluates our talent to ensure quality"} icon={karmaIcon} />
                 </div>
-
-                <div className='md:flex-1 mt-40 md:mt-24 flex flex-col items-center md:items-start text-center md:text-start gap-y-4'>
-                    <h1 className='w-[400px] leading-10 text-white text-3xl font-bold'>Build The Community Your Fans Will Love</h1>
-                    <p className='px-16 md:px-0 text-gray-200 text-lg'>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion. </p>
-
-                    <button
-                        className='mt-4 py-3 w-[250px] md:w-[200px] text-[#674baf] shadow-black shadow-2xl rounded-3xl bg-white hover:bg-transparent hover:text-white hover:outline hover:outline-white'
-                    >
-                        Register
-                    </button>
-                </div>
-            </div>
-
-            <div className='mt-24 md:mt-16 mb-10 md:mr-24 flex gap-x-5 justify-center md:justify-end items-center text-[20px] text-white'>
-                <button className='border-2 w-12 h-12 border-white hover:bg-white hover:text-[#674baf] rounded-full'><i className="fa-brands fa-facebook-f"></i></button>
-                <button className='border-2 w-12 h-12 border-white hover:bg-white hover:text-[#674baf] rounded-full'><i className="fa-brands fa-twitter"></i></button>
-                <button className='border-2 w-12 h-12 border-white hover:bg-white hover:text-[#674baf] rounded-full'><i className="fa-brands fa-instagram"></i></button>
+                <MiniCard className={"border-t-sky-500"} title={"Calculator"} para={"Uses data from past projects to provide better delivery estimates"} icon={calculatorIcon} />
             </div>
         </div>
     );
