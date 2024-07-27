5969dc414b8f5314bc5012b996e226facaa4593c
Manpreet Singh <manpreetmss400@gmail.com>
2024-07-27 17:14:02 +0530

    ping-single-column-coming-soon-page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Card from "./components/Card";
 export default function App() {
   return (
     <div
-      className="relative w-screen h-screen overflow-x-hidden"
+      className="relative w-screen h-screen overflow-x-hidden flex justify-center"
     >
       <Card />
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,39 +1,44 @@
-import supervisorIcon from '../images/icon-supervisor.svg';
-import calculatorIcon from '../images/icon-calculator.svg';
-import karmaIcon from '../images/icon-karma.svg';
-import teamBuilderIcon from '../images/icon-team-builder.svg';
-
-
-function MiniCard({ title, para, icon, className }) {
-    return (
-        <div className={`space-y-4 md:space-y-3 md:w-[300px] p-8 md:pr-4 md:pb-2 border-t-4 rounded-xl shadow-2xl ${className}`}>
-            <h2 className='text-2xl md:text-sm font-semibold'>{title}</h2>
-            <p className='text-gray-500 md:text-xs pr-20 md:pr-4'>{para}</p>
-            <div className='flex justify-end pr-4 pt-8 md:py-4'>
-                <img src={icon} alt="icon" className='h-20 md:h-12' />
-            </div>
-        </div>
-    );
-}
+import img from '../images/logo.svg';
+import illustrationDashboard from '../images/illustration-dashboard.png';
 
 function Card() {
     return (
-        <div className="pt-20 md:pt-8 px-8 md:px-0 font-poppins md:flex md:flex-col md:items-center">
-            <h1 className="text-2xl space-y-2 my-6 md:m-4">
-                <span className="block text-gray-500">Reliable, efficient delivery</span>
-                <span className="font-bold">Powered by Technology</span>
-            </h1>
-
-            <p className="md:text-center md:w-[700px] text-xl md:text-sm text-gray-500">Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
-
-            <div className='mt-36 md:mt-0 pb-16 space-y-8 md:flex md:items-center md:gap-8'>
-                <MiniCard className={"border-t-sky-500"} title={"Supervisor"} para={"Monitors activity to identify project roadblocks"} icon={supervisorIcon} />
-                <div className='space-y-4'>
-                    <MiniCard className={"border-t-red-500"} title={"Team Builder"} para={"Scans our talent network to create the optimal team for your project"} icon={teamBuilderIcon} />
-                    <MiniCard className={"border-t-yellow-500"} title={"Karma"} para={"Regularly evaluates our talent to ensure quality"} icon={karmaIcon} />
-                </div>
-                <MiniCard className={"border-t-sky-500"} title={"Calculator"} para={"Uses data from past projects to provide better delivery estimates"} icon={calculatorIcon} />
+        <div className='flex flex-col items-center px-8 md:w-[75%]'>
+            <div className='mt-28 md:mt-16 mb-8'>
+                <img src={img} alt="logo" />
             </div>
+
+            <h1 className='text-gray-500 text-3xl'>We are launching <span className='text-black'>soon!</span></h1>
+
+            <p className='my-4 tesxt-md'>Subscribe to get notified</p>
+
+            <div className="flex flex-col md:flex-row w-full md:w-[600px] px-8 mt-4 gap-4">
+                <input className='w-full md:w-[500px] pl-8 border py-2 rounded-3xl' placeholder='Your email address' type="email" name="email" id="email" />
+                <button className='w-full md:w-[200px]  text-white text-sm font-semibold border py-3 rounded-3xl bg-blue-600 hover:bg-blue-500'>Notify Me</button>
+            </div>
+
+            <div className='mt-28 md:mt-16'>
+                <img src={illustrationDashboard} alt="illustratioin-dashboard" />
+            </div>
+
+
+            <div className='my-24 md:my-16 flex justify-center gap-4 items-center text-blue-500 text-xl md:text-5xl'>
+                <a className='hover:text-white hover:bg-blue-500 flex justify-center items-center rounded-full border border-gray-300 w-8 h-8 md:w-16 md:h-16 p-1 pt-[5px] pl-[9px] md:pt-[5px] md:pl-[15px]' href="#">
+                    <i class="w-full h-full fa-brands fa-facebook-f"></i>
+                </a>
+                <a className='hover:text-white hover:bg-blue-500 flex justify-center items-center rounded-full border border-gray-300 w-8 h-8 md:w-16 md:h-16 p-1 pt-[4px] pl-[4px] md:pt-[5px] md:pl-[5px]' href="#">
+                    <i class="fa-brands fa-twitter"></i>
+                </a>
+                <a className='hover:text-white hover:bg-blue-500 flex justify-center items-center rounded-full border border-gray-300 w-8 h-8 md:w-16 md:h-16 p-1 pt-[5px] pl-[4px]' href="#">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+            </div>
+
+
+            <footer className='text-gray-500 my-8 md:pb-16'>
+                &copy; Copyright Ping. All rights reserved.
+            </footer>
+
         </div>
     );
 }
